2deb7e226cd03ebf2021ee38bfbaa7764192dc10
2016jtm2089 <vyshnavdutt.fiitjee@gmail.com>
2016-09-12 10:39:01 +0530

    hiiii

---
+++ command.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(int argc, char *argv[])   // 
+{
+if(argc<=1) 
+{
+   printf("Arguments passed through command line " 
+          "not > 1");
+   
+}
+
+return 0;
+}
